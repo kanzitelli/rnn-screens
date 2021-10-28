4c3133d56bec147ae471a9f7b69bd9b9a0f87680
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-10-28 19:43:21 +0200

    better exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,4 @@
 import * as React from 'react';
 
-import {Provider, ScreenLayoutsWithoutName, Screens} from './screens';
-import {BottomTabs, Component, Root, Stack, StackMany} from './layouts';
-
-function generateRNNScreens<ScreenName extends string = string>(
-  screens: ScreenLayoutsWithoutName<ScreenName>,
-  withProviders: Provider[] = [],
-) {
-  return new Screens<ScreenName>(screens, withProviders);
-}
-
-export {generateRNNScreens, Root, BottomTabs, Stack, StackMany, Component};
+export * from './layouts';
+export * from './screens';
