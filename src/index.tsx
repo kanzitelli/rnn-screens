9f481b7088fa0d05c9e9745fe04a4994f015cf27
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-10-28 04:15:22 +0200

    navigation methods

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import {ComponentProvider} from 'react-native';
+import pipe from 'lodash/flowRight';
 import {
   Options,
   Layout,
@@ -9,6 +11,7 @@ import {
   Constants,
   Navigation,
   NavigationConstants,
+  NavigationFunctionComponent,
 } from 'react-native-navigation';
 
 // ==========
@@ -24,34 +27,97 @@ const StackMany = (children?: LayoutStackChildren[], options?: Options): Layout 
 });
 const Stack = (c: LayoutStackChildren, options?: Options): Layout => StackMany([c], options);
 const Component = <P,>(component: LayoutComponent<P>): Layout => ({component});
-export {Root, BottomTabs, Stack, StackMany, Component};
 
 // ==============
 // | Navigation |
 // ==============
 
-class Nav {
-  private inited = false;
+type PVoid = Promise<void>;
+type ScreenInfo = {
+  component: NavigationFunctionComponent;
+  options: Options;
+};
+type ScreenLayouts<ScreenName extends string = string> = {
+  [key in ScreenName]: ScreenInfo;
+};
+
+class Nav<ScreenName extends string = string> {
   private N = Navigation;
+  private screens: ScreenLayouts<ScreenName>;
   C: NavigationConstants = Constants.getSync();
 
-  init = (): void => {
-    if (!this.inited) {
-      this.registerListeners();
+  constructor(screens: ScreenLayouts<ScreenName>, withWrappers: ComponentProvider[] = []) {
+    this.screens = screens;
 
-      this.inited = true;
+    this.registerScreens(screens, withWrappers);
+    this.registerListeners();
+  }
+
+  private registerScreens(
+    screens: ScreenLayouts<ScreenName>,
+    withWrappers: ComponentProvider[] = [],
+  ) {
+    this.screens = screens;
+
+    for (const [key, value] of Object.entries(screens)) {
+      const {component} = value as ScreenInfo;
+
+      this.N.registerComponent(
+        key,
+        pipe(withWrappers, () => component),
+        () => component,
+      );
     }
+  }
+
+  async push<T>(cId: string, name: ScreenName, passProps?: T, options?: Options): PVoid {
+    const s = this.screens[name];
+
+    await this.N.push(
+      cId,
+      Component({
+        name,
+        ...s,
+        passProps,
+        options: {
+          ...s.options,
+          ...options,
+        },
+      }),
+    );
+  }
+
+  pop = async (cId: string): PVoid => {
+    await this.N.pop(cId);
   };
 
-  private registerListeners = () => {
+  async show<T>(name: ScreenName, passProps?: T, options?: Options): PVoid {
+    const sl = this.screens[name];
+
+    this.N.showModal(
+      Stack(
+        Component({
+          name,
+          ...sl,
+          passProps,
+          options: {
+            ...sl.options,
+            ...options,
+          },
+        }),
+      ),
+    );
+  }
+
+  private registerListeners() {
     this.N.events().registerComponentWillAppearListener(() => {
       this.getConstants();
     });
-  };
+  }
 
-  private getConstants = async () => {
+  private getConstants() {
     this.C = Constants.getSync();
-  };
+  }
 }
 
-export default new Nav();
+export {Nav, Root, BottomTabs, Stack, StackMany, Component};
