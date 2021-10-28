8c555360f3f9574b2872e866a196af8ca2613e84
Batr Kanzitelli <kanzitdinov@gmail.com>
2021-10-28 14:52:15 +0200

    screens names

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {ComponentProvider} from 'react-native';
-import pipe from 'lodash/flowRight';
 import {
   Options,
   Layout,
@@ -13,6 +12,9 @@ import {
   NavigationConstants,
   NavigationFunctionComponent,
 } from 'react-native-navigation';
+import pipe from 'lodash/flowRight';
+import omit from 'lodash/omit';
+import merge from 'lodash/merge';
 
 // ==========
 // | Layout |
@@ -33,39 +35,53 @@ const Component = <P,>(component: LayoutComponent<P>): Layout => ({component});
 // ==============
 
 type PVoid = Promise<void>;
-type ScreenInfo<ScreenName extends string = string> = Omit<LayoutComponent, 'name'> & {
-  name?: ScreenName;
+
+type ScreenInfoWithoutName = Omit<LayoutComponent, 'name'> & {
   component: NavigationFunctionComponent;
 };
+type ScreenInfo<ScreenName extends string = string> = ScreenInfoWithoutName & {
+  name: ScreenName;
+};
+
 type ScreenLayouts<ScreenName extends string = string> = {
   [key in ScreenName]: ScreenInfo<ScreenName>;
+};
+type ScreenLayoutsWithoutName<ScreenName extends string = string> = {
+  [key in ScreenName]: ScreenInfoWithoutName;
 };
 
 class Nav<ScreenName extends string = string> {
   private N = Navigation;
-  private screens: ScreenLayouts<ScreenName>;
-  C: NavigationConstants = Constants.getSync();
+  Screens: ScreenLayouts<ScreenName>;
+  Constants: NavigationConstants = Constants.getSync();
 
-  constructor(screens: ScreenLayouts<ScreenName>, withProviders: ComponentProvider[] = []) {
-    this.screens = screens;
+  constructor(
+    screens: ScreenLayoutsWithoutName<ScreenName>,
+    withProviders: ComponentProvider[] = [],
+  ) {
+    this.Screens = screens as any;
+    // setting `name` for screens
+    Object.keys(screens).forEach(key => {
+      const snKey = key as ScreenName;
+      this.Screens[snKey] = {
+        ...screens[snKey],
+        name: snKey,
+      };
+    });
 
     this.registerScreens(withProviders);
     this.registerListeners();
   }
 
   async push<T>(cId: string, name: ScreenName, passProps?: T, options?: Options): PVoid {
-    const s = this.screens[name];
+    const s = omit(this.Screens[name], 'component');
 
     await this.N.push(
       cId,
       Component({
-        name,
         ...s,
         passProps,
-        options: {
-          ...s.options,
-          ...options,
-        },
+        options: merge(s.options, options),
       }),
     );
   }
@@ -75,33 +91,25 @@ class Nav<ScreenName extends string = string> {
   };
 
   async show<T>(name: ScreenName, passProps?: T, options?: Options): PVoid {
-    const sl = this.screens[name];
+    const s = omit(this.Screens[name], 'component');
 
     this.N.showModal(
       Stack(
         Component({
-          name,
-          ...sl,
+          ...s,
           passProps,
-          options: {
-            ...sl.options,
-            ...options,
-          },
+          options: merge(s.options, options),
         }),
       ),
     );
   }
 
   private registerScreens(withProviders: ComponentProvider[] = []) {
-    for (const [key, info] of Object.entries(this.screens)) {
-      const {component} = info as ScreenInfo;
-
-      // writing name as key
-      const sName = key as ScreenName;
-      this.screens[sName].name = sName;
+    for (const [, info] of Object.entries(this.Screens)) {
+      const {name, component} = info as ScreenInfo;
 
       this.N.registerComponent(
-        sName,
+        name,
         pipe(withProviders, () => component),
         () => component,
       );
@@ -115,17 +123,17 @@ class Nav<ScreenName extends string = string> {
   }
 
   private getConstants() {
-    this.C = Constants.getSync();
+    this.Constants = Constants.getSync();
   }
 }
 
-const n = new Nav<'one' | 'two'>({
-  one: {
-    component: () => <></>,
-  },
-  two: {
-    component: () => <></>,
-  },
-});
+// const n = new Nav<'one' | 'two'>({
+//   one: {
+//     component: () => <></>,
+//   },
+//   two: {
+//     component: () => <></>,
+//   },
+// });
 
 export {Nav, Root, BottomTabs, Stack, StackMany, Component};
