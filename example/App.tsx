c35dae8d3e4b31c52571bcf803a441425b7521b4
Batr Kanzitelli <kanzitdinov@gmail.com>
2022-01-11 00:50:50 +0100

    2 screens and navigation between them

--- example/App.tsx
+++ example/App.tsx
@@ -1,15 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,82 +9,74 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {generateRNNScreens, Root, Screen, ScreenComponent} from 'rnn-screens';
 
 const Section: React.FC<{
   title: string;
 }> = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
+  const textStyle = {color: isDarkMode ? '#fff' : '#000'};
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={S.sectionContainer}>
+      <Text style={[S.sectionTitle, textStyle]}>{title}</Text>
+      <View>{children}</View>
     </View>
   );
 };
 
-const App = () => {
+const Main: ScreenComponent = ({componentId}) => {
   const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {backgroundColor: isDarkMode ? '#000' : '#fff'};
+  const container = [S.f1, backgroundStyle];
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const push = () =>
+    screens.push<SettingsProps>(componentId, 'Settings', {type: 'push'});
+  const show = () => screens.show<SettingsProps>('Settings', {type: 'show'});
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={container}>
+        <Section title="Navigation">
+          <Button title="Push Settings" onPress={push} />
+          <Button title="Show Settings" onPress={show} />
+        </Section>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
+type SettingsProps = {
+  type: 'push' | 'show';
+};
+const Settings: ScreenComponent<SettingsProps> = ({componentId, type}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle = {backgroundColor: isDarkMode ? '#000' : '#fff'};
+  const container = [S.f1, backgroundStyle];
+
+  const goBack = async () => {
+    if (type === 'push') {
+      screens.pop(componentId);
+    }
+    if (type === 'show') {
+      screens.N.dismissAllModals();
+    }
+  };
+
+  return (
+    <SafeAreaView style={container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={container}>
+        <Section title={`Navigation (type = "${type}")`}>
+          <Button title="Go back" onPress={goBack} />
+        </Section>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const S = StyleSheet.create({
+  f1: {flex: 1},
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -102,14 +85,29 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: '600',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+});
+
+// ==============
+// | Navigation |
+// ==============
+const screens = generateRNNScreens({
+  Main: {
+    component: Main,
+    options: {
+      topBar: {
+        title: {text: 'Main'},
+      },
+    },
   },
-  highlight: {
-    fontWeight: '700',
+  Settings: {
+    component: Settings,
+    options: {
+      topBar: {
+        title: {text: 'Settings'},
+      },
+    },
   },
 });
+const App = () => Root(Screen(screens.get('Main')));
 
 export default App;
